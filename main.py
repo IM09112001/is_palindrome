8ec366d001954df10e154834b0708592f16d54fd
IM09112001 <imahamatdjanov@gmail.com>
2021-11-08 17:19:48 +0500

    1-git

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 isPalindrome = input("Write string: ")
-if isPalindrome == isPalindrome[::-1]:
-    print("This string is palindrome.")
-else: print("This string is not palindrome.")
+isPalindrome = isPalindrome.lower().rstrip(" ")
+def is_palindrome(isPalindrome):
+  return isPalindrome == isPalindrome[::-1]
+#     print("This string is palindrome.")
+# else: print("This string is not palindrome.")
+
+print(is_palindrome(isPalindrome))
